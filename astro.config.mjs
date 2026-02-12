c2d72feb6dc67a9e6f1fab5968e9499dcba0f9e1
FILLYLIN <140259526+FILLYLIN@users.noreply.github.com>
2026-02-12 21:47:14 +0800

    8

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mc.asahi-rokka.top',
-  base:'./',
   
   integrations: [
     starlight({
